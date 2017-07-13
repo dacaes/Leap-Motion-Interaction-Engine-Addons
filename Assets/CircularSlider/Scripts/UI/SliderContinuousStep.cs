7d2c131816709e7d93dd09143d82b5f5e9424509
Dani <daniel.c.estrella@gmail.com>
2017-07-13 12:18:24 +0200

    first commit

---
+++ Assets/CircularSlider/Scripts/UI/SliderContinuousStep.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using Leap.Unity.Interaction;
+
+public class SliderContinuousStep : MonoBehaviour
+{
+	private InteractionSlider slider1;
+	private InteractionCircularSlider slider2;
+	private int stepsH1, stepsV1, steps2;
+
+	private void Awake()
+	{
+		slider1 = GetComponent<InteractionSlider>();
+
+		if (slider1 != null)
+		{
+			stepsH1 = slider1.horizontalSteps;
+			stepsV1 = slider1.verticalSteps;
+			slider1.OnPress.AddListener(new UnityEngine.Events.UnityAction(OnPress));
+			slider1.OnUnpress.AddListener(new UnityEngine.Events.UnityAction(OnUnpress));
+		}
+
+		slider2 = GetComponent<InteractionCircularSlider>();
+
+		if (slider2 != null)
+		{
+			steps2 = slider2.steps;
+			slider2.OnPress.AddListener(new UnityEngine.Events.UnityAction(OnPress));
+			slider2.OnUnpress.AddListener(new UnityEngine.Events.UnityAction(OnUnpress));
+		}
+	}
+
+	public void OnPress()
+	{
+		if (slider1 != null)
+		{
+			slider1.verticalSteps = 0;
+			slider1.horizontalSteps = 0;
+		}
+
+		if (slider2 != null)
+		{
+			slider2.steps = 0;
+		}
+	}
+
+	public void OnUnpress()
+	{
+		if (slider1 != null)
+		{
+			slider1.verticalSteps = stepsV1;
+			slider1.horizontalSteps = stepsH1;
+		}
+		if (slider2 != null)
+		{
+			slider2.steps = steps2;
+		}
+
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Leap-Motion-Interaction-Engine-Addons
-Custom classes made on the top of the Interaction Engine.
+
+Daniel Castaño Estrella 2017
+daniel.c.estrella@gmail.com
+
+Custom classes made on the top of the Unity Leap Motion Interaction Engine.
+
+You must import Unity Core Assets and Leap Motion Interaction Engine Module for Unity. -> https://developer.leapmotion.com/unity#116
+
+*Scripts:*
+InteractionCircularSlider -> 1D slider working on the perimeter of a circle.
+
+SliderContinuousStep -> Apply to an object with a InteractionSldier or InteractionCircularSlider to be able to use continuous sliding and snap to steps on Unpress.
+
+---
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
